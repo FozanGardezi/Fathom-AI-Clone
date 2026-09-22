3de84538317fc547148a7c85e7731156857c65cd
Fozan Gardezi <fozangardezi@gmail.com>
2026-09-22 15:14:42 +0500

    Add deployed url to allowed host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
     DEBUG=(bool, False),
-    ALLOWED_HOSTS=(list, ["localhost", "127.0.0.1"]),
+    ALLOWED_HOSTS=(list, ["localhost", "127.0.0.1", "fathom-ai-clone-eq1u-git-master-fozangardezis-projects.vercel.app"]),
     CORS_ALLOWED_ORIGINS=(list, ["http://localhost:3000"]),
 )
 environ.Env.read_env(BASE_DIR / ".env")
